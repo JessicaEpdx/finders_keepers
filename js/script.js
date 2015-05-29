e13dca11cae29b2b3da0154196253798013ee4fc
Jessica Engel <student@epicodus.com>
2015-05-29 09:36:28 -0700

    will allow integer input

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 var findReplace = function(string, wordToChange, newWord){
-  var result = string
-  return result.replace(wordToChange, newWord)
+
+  if (typeof wordToChange === 'string'){
+
+    string = string.toLowerCase();
+    wordToChange = wordToChange.toLowerCase();
+    var result = string.replace(wordToChange, newWord);
+    return result.charAt(0).toUpperCase() + result.slice(1);
+
+  }else{
+    wordToChange = wordToChange.toString();
+    newWord = newWord.toString();
+    var result = string.replace(wordToChange, newWord);
+    return result.charAt(0).toUpperCase() + result.slice(1);s
+
+  }
 
 
 }
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,21 @@ describe('findReplace', function(){
   it("will replace one word in string with given new word", function(){
     expect(findReplace("Hey there", "Hey", "Hi")).to.equal("Hi there");
   });
+
+  it("will replace word regardless of case", function(){
+    expect(findReplace("Hey there", "hey", "Hi")).to.equal("Hi there");
+  });
+
+  it("will always capitalize string output", function(){
+    expect(findReplace("hey there", "hey", "hi")).to.equal("Hi there");
+  });
+
+  it("will accept numbers as input", function(){
+    expect(findReplace("8 monkeys", 8, 12)).to.equal("12 monkeys");
+  });
+
+  it("accept equality of words regardless of attached punctuation", function(){
+    expect(findReplace("hey! how are you?", "hey", "hi")).to.equal("Hi! how are you?");
+  });
+
 });
